7b76c7742c7d5823cbddf52e328847227b2a5689
wyo <seikimatu4649@gmail.com>
2025-01-29 20:49:15 +0900

    add level1 途中まで

--- level1/source.c
+++ level1/source.c
@@ -1,1 +1,20 @@
-#include <stdio.h>\nint\nmain(void) {\n  printf("Code, source!\n");\n  return (0x0);\n}
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+
+void run(void)
+{
+    fwrite("Good... Wait what?\n", 1, 19, stdout);
+    system("/bin/sh");
+    return;
+}
+
+void main(void)
+{
+    char buffer[64];
+
+    gets(buffer);
+    return;
+}
