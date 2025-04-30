4647ee15c56e43436aebc091cba87393db3f0af9
wyo <seikimatu4649@gmail.com>
2025-05-01 02:37:13 +0900

    add bonus3 ans

--- bonus3/source.c
+++ bonus3/source.c
@@ -1,1 +1,37 @@
-#include <stdio.h>\nint\nmain(void) {\n  printf("Code, source!\n");\n  return (0x0);\n}
+int main(int argc, char *argv[]) {
+  FILE *password_file;
+  unsigned int buffer1[16];
+  char buffer2[66];
+  char temp_byte;
+  int comparison_result;
+  unsigned int *buffer_ptr;
+
+  password_file = fopen("/home/user/end/.pass", "r");
+
+  // ファイルが開けなかった場合または引数の数が2でない場合はエラーを返す
+  if (password_file == NULL || argc != 2) {
+      return -1;
+  } else {
+      // パスワードファイルからデータを読み込む
+      fread(buffer1, 1, 66, password_file);
+
+      // buffer1 の指定された位置にあるバイトをゼロに設定
+      ((char *)buffer1)[atoi(argv[1])] = 0;
+
+      // 追加のデータを buffer2 に読み込む
+      fread(buffer2, 1, 65, password_file);
+
+      // ファイルを閉じる
+      fclose(password_file);
+
+      if (strcmp((char *)buffer1, argv[1]) == 0) {
+          // パスワードが一致した場合、シェルを起動
+          execl("/bin/sh", "sh", NULL);
+      } else {
+          // パスワードが一致しない場合、buffer2 の内容を表示
+          puts(buffer2);
+      }
+
+      return 0;
+  }
+}
--- bonus3/walkthrough.md
+++ bonus3/walkthrough.md
@@ -1,1 +1,22 @@
-# テスト解決手順\n\n手順をここに記載してください。
+# bonus3
+
+## 方針
+((char *)buffer1)[atoi(argv[1])] = 0;
+strcmp((char *)buffer1, argv[1]) == 0
+になるようなargv１を渡せばshが起動する。
+
+### ans
+buffer[0] == 0 && argv[1][0] == ‘\0’ ''だとうまくいく。
+なぜなら
+0: 数値のゼロ
+'': 空文字
+バイナリ: 0x00 vs 0x00
+つまり、./bonus3 '' でshが起動する。
+
+```
+bonus3@RainFall:~$ ./bonus3 ''
+$ cat /home/user/end/.pass
+3321b6f81659f9a71c76616f606e4b50189cecfea611393d5d649f75e157353c
+```
+
+## memo
